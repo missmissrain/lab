5a929502ca9fa6baf46f8ce2fee31fd13ea7f374
missmissrain <1974704197@qq.com>
2025-01-23 18:14:45 +0800

    Delete pt2onnx.py

--- pt2onnx.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# 加载 YOLO 模型
-model_path = "/home/hjk/桌面/tesk_rknn/yolov10n.pt"
-model = YOLO(model_path)
-
-# 导出为 ONNX 格式，批次大小为 1
-onnx_file_path = "/home/hjk/桌面/tesk_rknn/yolov10n.onnx"
-model.export(format="onnx", opset=12, dynamic=False)  # 明确禁用动态批次，并使用固定批次大小 1
